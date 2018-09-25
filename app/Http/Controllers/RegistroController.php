0c955ba887e3fac752df967ae48962390099cfe7
osvldovilla <osvldo35237@gmail.com>
2018-09-24 22:09:16 -0600

    Metodos del controlador
    Creación de metodos para las vistas

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -6,5 +6,41 @@ use Illuminate\Http\Request;
 
 class RegistroController extends Controller
 {
-    //
+  class CreateController extends Controller
+   {
+
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update($id)
+    {
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
 }
